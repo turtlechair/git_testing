5f76de680c110b0a1ec9b9212ad07031fa795bb2
Josh <turtlechair@gmail.com>
2026-06-02 06:59:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Program to add 3 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+sum = num1 + num2 + num3
+
+print(f"The sum of {num1}, {num2}, and {num3} is {sum}!")
